7ffc6ed05c264f806176fa80b56c6319bc096895
swathi <srinivasanswathi3@gmail.com>
2026-06-21 18:08:42 +0530

    Make API URL configurable via env variable for deployment

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client";
 let socket = null;
 
 export function connectSocket(token) {
-  socket = io("http://localhost:5000", {
+  socket = io(import.meta.env.VITE_API_URL?.replace("/api", "") || "http://localhost:5000", {
     auth: { token },
   });
   return socket;
